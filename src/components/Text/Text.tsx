676788697d64d59ccec9e5c6650d40848eda20c4
AndreaRettaroli <andrea.rettaroli@studio.unibo.it>
2025-05-17 15:56:05 +0200

    feat: wip refactor ui

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,44 +1,70 @@
-import React from 'react';
-import clsx from 'clsx';
+import React from "react";
+import clsx from "clsx";
 
-type FontWeight = 'semibold' | 'regular';
+type FontWeight = "semibold" | "regular";
 
-export type FontSize = 'xl' | 'l' | 'm' | 's' | 'xs';
+export type FontSize = "xxl" | "xl" | "l" | "m" | "s" | "xs";
 
-type FontColor = 'text' | 'player1' | 'player2' | 'penalty';
+type FontColor = "text" | "player1" | "player2" | "penalty";
 
 interface TextProps {
-    weight: FontWeight ;
-    size: FontSize;
-    color: FontColor;
+  weight: FontWeight;
+  size: FontSize;
+  color: FontColor;
 }
 
-export const Text: React.FC<React.PropsWithChildren<TextProps>> = ({ color, weight, size, children }) => (
-    <span className={clsx(getFontColorClass(color), getFontWeightClass(weight), getFontSizeClass(size))}>{ children }</span>
+export const Text: React.FC<React.PropsWithChildren<TextProps>> = ({
+  color,
+  weight,
+  size,
+  children,
+}) => (
+  <span
+    className={clsx(
+      getFontColorClass(color),
+      getFontWeightClass(weight),
+      getFontSizeClass(size)
+    )}
+  >
+    {children}
+  </span>
 );
 
-function getFontWeightClass (fontWeight: FontWeight): string {
-    switch (fontWeight) {
-        case 'semibold': return '-fw-sb';
-        case 'regular': return '-fw-r';
-    }
+function getFontWeightClass(fontWeight: FontWeight): string {
+  switch (fontWeight) {
+    case "semibold":
+      return "-fw-sb";
+    case "regular":
+      return "-fw-r";
+  }
 }
 
-function getFontSizeClass (fontSize: FontSize): string {
-    switch (fontSize) {
-        case 'xl': return '-fs-xl';
-        case 'l': return '-fs-l';
-        case 'm': return '-fs-m';
-        case 's': return '-fs-s';
-        case 'xs': return '-fs-xs';
-    }
+function getFontSizeClass(fontSize: FontSize): string {
+  switch (fontSize) {
+    case "xxl":
+      return "-fs-xxl";
+    case "xl":
+      return "-fs-xl";
+    case "l":
+      return "-fs-l";
+    case "m":
+      return "-fs-m";
+    case "s":
+      return "-fs-s";
+    case "xs":
+      return "-fs-xs";
+  }
 }
 
-function getFontColorClass (fontColor: FontColor): string {
-    switch (fontColor) {
-        case 'text': return '-c-text';
-        case 'player1': return '-c-player1-secondary';
-        case 'player2': return '-c-player2-secondary';
-        case 'penalty': return '-c-penalty';
-    }
+function getFontColorClass(fontColor: FontColor): string {
+  switch (fontColor) {
+    case "text":
+      return "-c-text";
+    case "player1":
+      return "-c-player1-secondary";
+    case "player2":
+      return "-c-player2-secondary";
+    case "penalty":
+      return "-c-penalty";
+  }
 }
