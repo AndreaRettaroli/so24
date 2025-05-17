cb518beb97c2c6d07a0ffe2d3add8597854c3301
AndreaRettaroli <andrea.rettaroli@studio.unibo.it>
2025-05-17 15:56:05 +0200

    feat: wip refactor ui

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,89 +1,115 @@
-import React from 'react';
-import clsx from 'clsx';
+import React from "react";
+import clsx from "clsx";
 
-type ButtonColors = 'gradient' | 'player1' | 'player2' | 'default' | 'transparent' | 'disabled';
+type ButtonColors =
+  | "gradient"
+  | "player1"
+  | "player2"
+  | "default"
+  | "transparent"
+  | "disabled";
 
 interface SharedButtonProps {
-    onClick?: () => void;
-    className?: string;
-    disabled?: boolean;
+  onClick?: () => void;
+  className?: string;
+  disabled?: boolean;
 }
 
 interface NormalButtonProps extends SharedButtonProps {
-    variant: 'normal';
-    borderOnly?: boolean; // default false
-    color: ButtonColors;
-    height?: 'large' | 'medium'; // default large
-    fullWidth?: boolean;
-    // icon
+  variant: "normal";
+  borderOnly?: boolean; // default false
+  color: ButtonColors;
+  height?: "large" | "medium"; // default large
+  fullWidth?: boolean;
+  // icon
 }
 
 interface RotatedButtonProps extends SharedButtonProps {
-    variant: 'rotated';
-    borderOnly?: boolean; // default false
-    color: Exclude<ButtonColors, 'transparent'>;
-    height?: 'large' | 'medium'; // default large
-    // icon only
+  variant: "rotated";
+  borderOnly?: boolean; // default false
+  color: Exclude<ButtonColors, "transparent">;
+  height?: "x-large" | "large" | "medium"; // default large
+  // icon only
 }
 
-export const Button: React.FC<React.PropsWithChildren<RotatedButtonProps | NormalButtonProps>> = (props) => (
-    <button className={clsx(getButtonClassesArray(props), props.className)} onClick={props.onClick} disabled={props.disabled}>
-        <div className={clsx('-df -a-ctr -j-c', props.variant === 'rotated' && '-dcol')}>{ props.children }</div>
-    </button>
+export const Button: React.FC<
+  React.PropsWithChildren<RotatedButtonProps | NormalButtonProps>
+> = (props) => (
+  <button
+    className={clsx(getButtonClassesArray(props), props.className)}
+    onClick={props.onClick}
+    disabled={props.disabled}
+  >
+    <div
+      className={clsx(
+        "-df -a-ctr -j-c",
+        props.variant === "rotated" && "-dcol"
+      )}
+    >
+      {props.children}
+    </div>
+  </button>
 );
 
-function getButtonClassesArray (props: RotatedButtonProps | NormalButtonProps): string[] {
-    const classes = [];
+function getButtonClassesArray(
+  props: RotatedButtonProps | NormalButtonProps
+): string[] {
+  const classes = [];
 
-    if (props.height === 'medium') {
-        classes.push('_medium');
-    } else {
-        classes.push('_large');
+  if (props.height === "medium") {
+    classes.push("_medium");
+  } else if (props.height === "x-large") {
+    classes.push("_x-large");
+  } else {
+    classes.push("_large");
+  }
+
+  if (props.variant === "normal") {
+    classes.push("player-button");
+
+    if (props.fullWidth) {
+      classes.push("-fw");
     }
+  }
 
-    if (props.variant === 'normal') {
-        classes.push('player-button');
+  if (props.variant === "rotated") {
+    classes.push("rotated-button");
+  }
 
-        if (props.fullWidth) {
-            classes.push('-fw');
-        }
+  if (props.borderOnly) {
+    if (props.variant === "normal") {
+      classes.push("button-border");
     }
+    classes.push("_border-only");
+  }
 
-    if (props.variant === 'rotated') {
-        classes.push('rotated-button');
+  classes.push(colorTypeToClass(props.color));
 
-        if (props.borderOnly) {
-            classes.push('_border-only');
-        }
-    }
-
-    classes.push(colorTypeToClass(props.color));
-
-    return classes;
+  return classes;
 }
 
-function colorTypeToClass (color: ButtonColors) {
-    switch (color) {
-        case 'player1': {
-            return '_player1';
-        }
-        case 'player2': {
-            return '_player2';
-        }
-        case 'default': {
-            return '_default';
-        }
-        case 'gradient': {
-            return '_gradient-border';
-        }
-        case 'transparent': {
-            return '_transparent';
-        }
-        case 'disabled': {
-            return '_disabled';
-        }
-        default: {
-            return '_default';
-        }
+function colorTypeToClass(color: ButtonColors) {
+  switch (color) {
+    case "player1": {
+      return "_player1";
     }
+    case "player2": {
+      return "_player2";
+    }
+    case "default": {
+      return "_default";
+    }
+    case "gradient": {
+      return "_gradient-border";
+    }
+    case "transparent": {
+      return "_transparent";
+    }
+    case "disabled": {
+      return "_disabled";
+    }
+    default: {
+      return "_default";
+    }
+  }
 }
--- src/screens/Game.tsx
+++ src/screens/Game.tsx
@@ -1,205 +1,288 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
-import { Button } from '@Components/Button/Button';
-import { Text } from '@Components/Text/Text';
-import { TotalPoints } from '@Components/TotalPoints/TotalPoints';
-import { PointHistory } from '@Components/PointHistory/PointHistory';
-import { Icon } from '@Components/Icon/Icon';
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { Button } from "@Components/Button/Button";
+import { Text } from "@Components/Text/Text";
+import { TotalPoints } from "@Components/TotalPoints/TotalPoints";
+import { PointHistory } from "@Components/PointHistory/PointHistory";
+import { Icon } from "@Components/Icon/Icon";
 import {
-    useGameStateCurrentPlayer,
-    useGameStatePlayer1,
-    useGameStatePlayer2,
-    useGameStateCurrentPlayerTurn,
-    useGameStateCountdown,
-} from '@Hooks/useGameStateStore';
-import { useGameStateContext } from '@Hooks/useGameStateContext';
-import clsx from 'clsx';
+  useGameStateCurrentPlayer,
+  useGameStatePlayer1,
+  useGameStatePlayer2,
+  useGameStateCurrentPlayerTurn,
+  useGameStateCountdown,
+} from "@Hooks/useGameStateStore";
+import { useGameStateContext } from "@Hooks/useGameStateContext";
+import clsx from "clsx";
 
 export const GameScreen: React.FC = () => {
-    const { t } = useTranslation();
-    const currentPlayer = useGameStateCurrentPlayer();
-    const currentPlayerTurn = useGameStateCurrentPlayerTurn();
-    const { addPoints, undoPoints, stopGame, nextTurn, toggleNoDice } = useGameStateContext();
-    const { value: seconds } = useGameStateCountdown();
-    const player1 = useGameStatePlayer1();
-    const player2 = useGameStatePlayer2();
+  const { t } = useTranslation();
+  const currentPlayer = useGameStateCurrentPlayer();
+  const currentPlayerTurn = useGameStateCurrentPlayerTurn();
+  const { addPoints, undoPoints, stopGame, nextTurn, toggleNoDice } =
+    useGameStateContext();
+  const { value: seconds } = useGameStateCountdown();
+  const player1 = useGameStatePlayer1();
+  const player2 = useGameStatePlayer2();
 
-    return (
-        <div className="-h-100vh">
-            <div style={{ position: 'absolute', top: 0, width: '100%', zIndex: '9999' }}>
-                <Button variant="normal" color="player2" fullWidth className="-t-r180dg" onClick={() => toggleNoDice('player2')}>
-                    {
-                        player2.noDice
-                            ? <Icon icon="die" alt={t('noDice')} width="24px" height="24px" />
-                            : <Icon icon="dieDisabled" alt={t('noDice')} width="24px" height="24px" />
-                    }
-                    <Text color="text" size="s" weight="regular">
-                        { player2.noDice ? t('haveDice') : t('noDice') }
-                    </Text>
-                </Button>
-                {
-                    currentPlayer === 'player1' && (
-                        <div className="-t-r180dg">
-                            <TotalPoints points={player2.points} player="player2" />
-                        </div>
-                    )
-                }
-            </div>
-            <div
-                style={{ position: 'relative', width: '100vw', height: '100vh' }}
-                className={clsx(currentPlayer === 'player2' && '-t-r180dg')}
+  return (
+    <div className="-h-100vh">
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          width: "100%",
+          zIndex: "100",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Button
+          variant="normal"
+          color="player2"
+          className="-t-r180dg"
+          onClick={() => toggleNoDice("player2")}
+        >
+          {player2.noDice ? (
+            <Icon icon="die" alt={t("noDice")} width="24px" height="24px" />
+          ) : (
+            <Icon
+              icon="dieDisabled"
+              alt={t("noDice")}
+              width="24px"
+              height="24px"
+            />
+          )}
+          <Text color="text" size="s" weight="regular">
+            {player2.noDice ? t("haveDice") : t("noDice")}
+          </Text>
+        </Button>
+        {currentPlayer === "player1" && (
+          <div className="-t-r180dg">
+            <TotalPoints points={player2.points} player="player2" />
+          </div>
+        )}
+      </div>
+      <div
+        style={{ position: "relative", width: "100vw", height: "100vh" }}
+        className={clsx(currentPlayer === "player2" && "-t-r180dg")}
+      >
+        <div>
+          <div
+            style={{
+              position: "absolute",
+              top: "10%",
+              alignItems: "center",
+              justifyContent: "center",
+              display: "flex",
+              flexDirection: "row",
+              width: "100vw",
+              zIndex: "-1", // this is required because of width
+            }}
+          >
+            <>
+              <Text size="s" color="text" weight="regular">
+                {t("turn")}
+              </Text>
+              <Text size="s" color="text" weight="regular">
+                {" " + currentPlayerTurn}/24
+              </Text>
+            </>
+          </div>
+          <div
+            style={{
+              position: "absolute",
+              top: "25%",
+              left: "50%",
+              transform: "translateX(-50%)",
+            }}
+          >
+            <Button
+              variant="rotated"
+              color={currentPlayer}
+              height="x-large"
+              borderOnly
+              onClick={() => stopGame()}
             >
-                <div>
-                    <div style={{ position: 'absolute', top: '23%', left: '10%' }}>
-                        <div
-                            className={clsx(currentPlayer === 'player1' ? '-bg-player1' : '-bg-player2')}
-                            style={
-                                { position: 'absolute', left: 'calc(-0% - 10vw)', top: 'calc(47% + 1px)', width: '10vw', height: '3px' }
-                            }
-                        />
-                        <Button
-                            variant="rotated"
-                            color={currentPlayer}
-                            borderOnly
-                            onClick={() => stopGame()}
-                        >
-                            <Icon icon="pause" alt="Pause" />
-                            <Text color="text" size="m" weight="regular">
-                                { seconds.toString() }
-                            </Text>
-                        </Button>
-                    </div>
-                    <div style={
-                        {
-                            position: 'absolute',
-                            top: '26%',
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            display: 'flex',
-                            flexDirection: 'column',
-                            width: '100vw',
-                            zIndex: '-1', // this is required because of width
-                        }
-                    }
-                    >
-                        <Text size="s" color="text" weight="regular">{ t('turn') }</Text>
-                        <Text size="s" color="text" weight="regular">{ currentPlayerTurn }/24</Text>
-                    </div>
-                    <div style={{ position: 'absolute', top: '23%', right: '10%' }}>
-                        <div
-                            className={clsx(currentPlayer === 'player1' ? '-bg-player1' : '-bg-player2')}
-                            style={
-                                { position: 'absolute', right: 'calc(0% - 10vw)', top: 'calc(47% + 1px)', width: '10vw', height: '3px' }
-                            }
-                        />
-                        <Button
-                            variant="rotated"
-                            color={currentPlayer}
-                            borderOnly
-                            onClick={() => nextTurn()}
-                        >
-                            <Text color="text" size="m" weight="regular">
-                                { t('pass') }
-                            </Text>
-                        </Button>
-                    </div>
-                </div>
-                <div style={
-                    {
-                        position: 'absolute',
-                        top: '45%',
-                        display: 'flex',
-                        flexDirection: 'row',
-                        alignItems: 'center',
-                        width: '100vw',
-                        justifyContent: 'space-around',
-                    }
-                }
-                >
-                    <TotalPoints points={currentPlayer === 'player1' ? player1.points : player2.points} player={currentPlayer} />
-                    <PointHistory pointHistory={currentPlayer === 'player1' ? player1.pointHistory : player2.pointHistory} />
-                    <Button height="medium" variant="rotated" color="default" borderOnly onClick={() => undoPoints(currentPlayer)}>
-                        <Icon icon="annul" alt="Annul" />
-                    </Button>
-                </div>
-                <div style={
-                    {
-                        position: 'absolute',
-                        top: 'calc(60% + 20px)',
-                        left: '15%',
-                    }
-                }
-                >
-                    <Button height="medium" variant="rotated" color={currentPlayer} onClick={() => addPoints(currentPlayer, 1)}>
-                        <Text color="text" size="m" weight="regular">1</Text>
-                    </Button>
-                </div>
-                <div style={
-                    {
-                        position: 'absolute',
-                        top: 'calc(60% + 20px)',
-                        left: '45%',
-                    }
-                }
-                >
-                    <Button height="medium" variant="rotated" color={currentPlayer} onClick={() => addPoints(currentPlayer, 3)}>
-                        <Text color="text" size="m" weight="regular">3</Text>
-                    </Button>
-                </div>
-                <div style={
-                    {
-                        position: 'absolute',
-                        top: 'calc(60% + 20px)',
-                        left: '75%',
-                    }
-                }
-                >
-                    <Button height="medium" variant="rotated" color={currentPlayer} onClick={() => addPoints(currentPlayer, 5)}>
-                        <Text color="text" size="m" weight="regular">5</Text>
-                    </Button>
-                </div>
-                <div style={
-                    {
-                        position: 'absolute',
-                        top: 'calc(70% + 5px)',
-                        left: 'calc(24% + 20px)',
-                    }
-                }
-                >
-                    <Button height="medium" variant="rotated" color={currentPlayer} onClick={() => addPoints(currentPlayer, 2)}>
-                        <Text color="text" size="m" weight="regular">2</Text>
-                    </Button>
-                </div>
-                <div style={
-                    {
-                        position: 'absolute',
-                        top: 'calc(70% + 5px)',
-                        right: 'calc(21% + 20px)',
-                    }
-                }
-                >
-                    <Button height="medium" variant="rotated" color={currentPlayer} onClick={() => addPoints(currentPlayer, 4)}>
-                        <Text color="text" size="m" weight="regular">4</Text>
-                    </Button>
-                </div>
-            </div>
-            <div style={{ position: 'absolute', bottom: 0, width: '100%' }}>
-                {
-                    currentPlayer === 'player2' && (
-                        <TotalPoints points={player1.points} player="player1" />
-                    )
-                }
-                <Button variant="normal" color="player1" fullWidth onClick={() => toggleNoDice('player1')}>
-                    {
-                        player1.noDice
-                            ? <Icon icon="die" alt={t('noDice')} width="24px" height="24px" />
-                            : <Icon icon="dieDisabled" alt={t('noDice')} width="24px" height="24px" />
-                    }
-                    <Text color="text" size="s" weight="regular">
-                        { player1.noDice ? t('haveDice') : t('noDice') }
-                    </Text>
-                </Button>
-            </div>
+              <Icon icon="pause" alt="Pause" />
+              <Text color="text" size="xxl" weight="regular">
+                {seconds.toString()}
+              </Text>
+            </Button>
+          </div>
+          <div
+            style={{
+              position: "absolute",
+              top: "calc(88% + 1px)",
+              zIndex: "1000",
+              left: "50%",
+              transform: "translateX(-50%)",
+              alignItems: "center",
+              justifyContent: "center",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Button
+              variant="rotated"
+              color={currentPlayer}
+              borderOnly
+              onClick={() => nextTurn()}
+            >
+              <Text color="text" size="m" weight="regular">
+                {t("pass")}
+              </Text>
+            </Button>
+          </div>
         </div>
-    );
+        <div
+          style={{
+            position: "absolute",
+            top: "45%",
+            justifyContent: "center",
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            width: "100vw",
+          }}
+        >
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            <PointHistory
+              pointHistory={
+                currentPlayer === "player1"
+                  ? player1.pointHistory
+                  : player2.pointHistory
+              }
+            />
+            <div style={{ position: "absolute", left: "60%" }}>
+              <Button
+                height="medium"
+                variant="rotated"
+                color="default"
+                borderOnly
+                onClick={() => undoPoints(currentPlayer)}
+              >
+                <Icon icon="annul" alt="Annul" />
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div
+          id="button-container"
+          style={{
+            position: "absolute",
+            top: "calc(60% + 20px)",
+            width: "100%",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-around",
+              width: "80%",
+              margin: "0 auto",
+            }}
+          >
+            <Button
+              height="medium"
+              variant="normal"
+              color={currentPlayer}
+              borderOnly
+              onClick={() => addPoints(currentPlayer, 1)}
+            >
+              <Text color="text" size="l" weight="regular">
+                1
+              </Text>
+            </Button>
+
+            <Button
+              height="medium"
+              variant="normal"
+              color={currentPlayer}
+              borderOnly
+              onClick={() => addPoints(currentPlayer, 3)}
+            >
+              <Text color="text" size="l" weight="regular">
+                2
+              </Text>
+            </Button>
+
+            <Button
+              height="medium"
+              variant="normal"
+              color={currentPlayer}
+              borderOnly
+              onClick={() => addPoints(currentPlayer, 5)}
+            >
+              <Text color="text" size="l" weight="regular">
+                3
+              </Text>
+            </Button>
+
+            <Button
+              height="medium"
+              variant="normal"
+              color={currentPlayer}
+              borderOnly
+              onClick={() => addPoints(currentPlayer, 2)}
+            >
+              <Text color="text" size="l" weight="regular">
+                4
+              </Text>
+            </Button>
+
+            <Button
+              height="medium"
+              variant="normal"
+              color={currentPlayer}
+              borderOnly
+              onClick={() => addPoints(currentPlayer, 4)}
+            >
+              <Text color="text" size="l" weight="regular">
+                5
+              </Text>
+            </Button>
+          </div>
+        </div>
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          bottom: 0,
+          width: "100%",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Button
+          variant="normal"
+          color="player1"
+          onClick={() => toggleNoDice("player1")}
+        >
+          {player1.noDice ? (
+            <Icon icon="die" alt={t("noDice")} width="24px" height="24px" />
+          ) : (
+            <Icon
+              icon="dieDisabled"
+              alt={t("noDice")}
+              width="24px"
+              height="24px"
+            />
+          )}
+          <Text color="text" size="s" weight="regular">
+            {player1.noDice ? t("haveDice") : t("noDice")}
+          </Text>
+        </Button>
+
+        {currentPlayer === "player2" && (
+          <TotalPoints points={player1.points} player="player1" />
+        )}
+      </div>
+    </div>
+  );
 };
